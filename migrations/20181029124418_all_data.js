db78350f9e6329cd90ee9cc9a7f200fd032cbe7f
Ryan Borgstrom <rkborgstrom@mix.wvu.edu>
2019-06-17 15:49:41 -0600

    Add files via upload

---
+++ migrations/20181029124418_all_data.js
@@ -0,0 +1,19 @@
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable('all_data', (table) => {
+        table.increments('id').primary();
+        table.string('full_name');
+        table.string('mountain');
+        table.string('message');
+        table.integer('passengers');
+        table.string('time_leaving');
+        table.date('date');
+        table.time('time')
+        table.string('phone_number');
+        table.string('insta');
+        table.date('today');
+    });
+};
+
+exports.down = function (knex, Promise) {
+    return knex.schema.dropTable('all_data');
+};
